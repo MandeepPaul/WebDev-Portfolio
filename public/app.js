f281daa12651e0c5c559deadacd4e48854c163f6
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2023-05-19 01:06:03 -0400

    Implemented_Handlebars
    -Successfully added handlebars
    -added button that toggle between dark and light mode.

---
+++ public/app.js
@@ -0,0 +1,31 @@
+// Button
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleButton");
+    //console.log(toggleButton); Test
+  
+      
+
+// Check the initial theme preference
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const siteTheme = localStorage.getItem('theme');
+
+// Apply the initial theme preference
+if (siteTheme === 'dark' || (siteTheme === null && systemTheme)) {
+  document.documentElement.classList.add('dark');
+}
+
+// Toggle theme function
+function themeSwitch() {
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem("theme", "dark");
+  }
+  //console.log("Click!!"); Test
+}
+
+// Add click event listener to the toggle button
+toggleButton.addEventListener('click', themeSwitch);
+});
