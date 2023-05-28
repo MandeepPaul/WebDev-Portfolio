aef2bea0cece7a478a1d57a4407f969b30d33f51
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2023-05-28 18:12:19 -0400

    Separate Routes
    Created a route module and separate endpoints.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require("express"),
     path = require('path'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
-    Users = require('./models/userInfo');
+    User = require('./models/userInfo'),
+    routes = require("./routes/route")(app);
 
     // load env variables
 const dotenv = require("dotenv");
@@ -34,46 +35,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
-// Endpoints
-app.get("/", function(req, res){
-    res.render("about");
-})
-
-app.get("/skills", function(req, res){
-    res.render("skills");
-})
-
-app.get("/projects", function(req, res){
-    res.render("projects");
-})
-
-app.get("/education", function(req, res){
-    res.render("education");
-})
-
-app.get("/contact", function(req, res){
-    res.render("contact");
-})
-
-app.post("/submit", function(req, res) {
-    if (!req.body.uname || !req.body.uemail || !req.body.umessage) { //Making sure that required fields are not empty!
-      res.render('contact', { message: 'Required fields cannot be empty!'});
-    } 
-    else {
-      const user = new Users(req.body);
-    //   console.log(user.uname);
-    //   console.log(user.uemail);
-    //   console.log(user.umessage);
-      user.save()
-        .then(() => {
-          res.render('contact', { message: 'Thanks for contacting! Will get in touch soon.' });
-        })
-        .catch(err => {
-          res.status(400).json({ error: err });
-        });
-    }
-  });
-  
+app.use("/", routes);
 
 app.listen(app.get("port"), function () {
 	console.log("Express started on http://localhost:" + app.get("port") +"; press Ctrl-C to terminate." );
